1ba6f0c201e2abaf96cb1868593a074ab88999bb
frank <franktian424@qq.com>
2021-05-23 23:50:39 +0800

    finish ensemble-sparse

---
+++ mbpo_pytorch/models/causal_util.py
@@ -0,0 +1,146 @@
+import torch
+import torch.nn as nn
+import math
+import numpy as np
+
+from mbpo_pytorch.models.ensemble_util import EnsembleLayer
+from mbpo_pytorch.models.sparse_uitl import EnsembleSparseLayer
+from mbpo_pytorch.thirdparty.trace_expm import trace_expm
+from collections import OrderedDict
+
+from typing import List, Dict, Union, Optional
+
+from abc import ABC
+
+class EnsembleCausalMLP(nn.Module):
+    def __init__(self,
+                 hidden_dims,
+                 node_num,
+                 activation='tanh',
+                 last_activation='identity',
+                 ensemble_size=7,
+                 biases=None):
+        super(EnsembleCausalMLP, self).__init__()
+        self.hidden_dims = hidden_dims.copy() + [2]       # last layer is (mean, variance)
+        biases = [True] * (len(self.hidden_dims) - 1) if biases is None else biases.copy()
+        self.activation = getattr(torch, activation)
+        # setattr in __init__.py of models
+        self.last_activation = getattr(torch, last_activation)
+        self.node_num = node_num
+        self.ensemble_size = ensemble_size
+
+        # fc1: variable splitting for l1
+        self.fc1_pos = EnsembleLayer(node_num, node_num * self.hidden_dims[0],
+                                     ensemble_size=ensemble_size, bias=biases[0])
+        self.fc1_neg = EnsembleLayer(node_num, node_num * self.hidden_dims[0],
+                                     ensemble_size=ensemble_size, bias=biases[0])
+        # bound是对于fc1而言的，对于i==j，要求weight只能为0
+        # 对于其他的参数，要求weight至少0，因此有两个weight，分别代表正和负
+        self.fc1_pos.weight.bounds = self._bounds()
+        self.fc1_neg.weight.bounds = self._bounds()
+        # fc2: local linear layers
+        layers = []
+        if 1 < len(self.hidden_dims):
+            for l in range(len(self.hidden_dims) - 1):
+                layers.append(EnsembleSparseLayer(node_num, self.hidden_dims[l], self.hidden_dims[l + 1],
+                                                  ensemble_size=ensemble_size, bias=biases[l]))
+        self.fc2 = nn.ModuleList(layers)
+
+    def set_bound(self, bound):
+        self.fc1_pos.weight.bounds = bound
+        self.fc1_neg.weight.bounds = bound
+
+    def _bounds(self):
+        bounds = []
+        for k in range(self.ensemble_size):
+            for j in range(self.node_num):
+                for m in range(self.hidden_dims[0]):
+                    for i in range(self.node_num):
+                        if i == j:
+                            bound = (0, 0)
+                        else:
+                            bound = (0, None)
+                        bounds.append(bound)
+        return bounds
+
+    def forward(self, x):
+        # x size: batch-size, ensemble-size, node-num
+        x = self.fc1_pos(x) - self.fc1_neg(x)  # [n, d * m1]
+        # x size: batch-size, ensemble-size, (node-num * dims[0])
+        x = x.view(-1, self.ensemble_size, self.node_num, self.hidden_dims[0])  # [n, d, m1]
+        # x size: batch-size, ensemble-size, node-num, dims[0]
+        for fc in self.fc2:
+            x = self.activation(x)
+            x = fc(x)
+        # x size: batch-size, ensemble-size, node-num, 2
+        x = self.last_activation(x)
+        return x
+
+    def h_func(self):
+        """Constrain 2-norm-squared of fc1 weights along m1 dim to be a DAG"""
+        d = self.dims[0]
+        fc1_weight = self.fc1_pos.weight - self.fc1_neg.weight  # [e, j * m1, i]
+        fc1_weight = fc1_weight.view(self.ensemble_size, d, -1, d)  # [e, j, m1, i]
+        A = torch.sum(fc1_weight * fc1_weight, dim=2)
+        A = A.transpose(-1, -2)         # transpose
+        h_list = [trace_expm(A[idx]) - d for idx in range(self.ensemble_size)]
+        h = sum(h_list)     # (Zheng et al. 2018)
+        # A different formulation, slightly faster at the cost of numerical stability
+        # M = torch.eye(d) + A / d  # (Yu et al. 2019)
+        # E = torch.matrix_power(M, d - 1)
+        # h = (E.t() * M).sum() - d
+        return h
+
+    def l2_reg(self):
+        """Take 2-norm-squared of all parameters"""
+        reg = 0.
+        fc1_weight = self.fc1_pos.weight - self.fc1_neg.weight  # [j * m1, i]
+        reg += torch.sum(fc1_weight ** 2)
+        for fc in self.fc2:
+            reg += torch.sum(fc.weight ** 2)
+        return reg
+
+    def fc1_l1_reg(self):
+        """Take l1 norm of fc1 weight"""
+        reg = torch.sum(self.fc1_pos.weight + self.fc1_neg.weight)
+        return reg
+
+    @torch.no_grad()
+    def fc1_to_adj(self) -> np.ndarray:  # [e, j * m1, i] -> [i, j]
+        """Get W from fc1 weights, take 2-norm over m1 dim"""
+        d = self.dims[0]
+        fc1_weight = self.fc1_pos.weight - self.fc1_neg.weight  # [e, j * m1, i]
+        fc1_weight = fc1_weight.view(self.ensemble_size, d, -1, d)  # [e, j, m1, i]
+        A = torch.sum(fc1_weight * fc1_weight, dim=2)
+        A = A.transpose(-1, -2)         # transpose
+        W = torch.sqrt(A)  # [e, i, j]
+        W = W.cpu().detach().numpy()  # [e, i, j]
+        return W
+
+
+class EnsembleCausalModel(EnsembleCausalMLP):
+    def __init__(self, state_dim: int, action_dim: int, reward_dim: int, hidden_dims: List[int],
+                 output_state_dim=None, ensemble_size=7):
+        output_state_dim = output_state_dim or state_dim
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.output_state_dim = output_state_dim
+        self.reward_dim = reward_dim
+        self.node_num = state_dim + action_dim + output_state_dim + reward_dim
+        self.ensemble_size = ensemble_size
+        super(EnsembleCausalModel, self).__init__(hidden_dims, self.node_num, ensemble_size=ensemble_size)
+
+        self.load_model_bound()
+
+    def load_model_bound(self):
+        bounds = []
+        for k in range(self.ensemble_size):
+            for i in range(self.node_num):              # states + actions + states + reward(1)
+                for m in range(self.hidden_dims[0]):
+                    for j in range(self.node_num):
+                        if i == j:
+                            bound = (0, 0)
+                        else:
+                            bound = (0, None)
+                        bounds.append(bound)
+        return bounds
--- mbpo_pytorch/models/sparse_uitl.py
+++ mbpo_pytorch/models/sparse_uitl.py
@@ -3,26 +3,29 @@ import torch.nn as nn
 import math
 import numpy as np
 
+from mbpo_pytorch.models.ensemble_util import EnsembleLayer
+from mbpo_pytorch.thirdparty.trace_expm import trace_expm
 from collections import OrderedDict
 
 from typing import List, Dict, Union, Optional
 
-
 from abc import ABC
 
 
-class EnsembleLayer(nn.Module):
-    def __init__(self, input_features, output_features, ensemble_size=7, bias=True):
-        super(EnsembleLayer, self).__init__()
+class EnsembleSparseLayer(nn.Module):
+    def __init__(self, input_features, output_features, node_num, ensemble_size=7, bias=True):
+        super(EnsembleSparseLayer, self).__init__()
+        self.node_num = node_num
         self.ensemble_size = ensemble_size
         self.input_features = input_features
         self.output_features = output_features
 
         self.weight = nn.Parameter(torch.Tensor(ensemble_size,
+                                                node_num,
                                                 input_features,
                                                 output_features))
         if bias:
-            self.bias = nn.Parameter(torch.Tensor(ensemble_size, output_features))
+            self.bias = nn.Parameter(torch.Tensor(ensemble_size, node_num, output_features))
         else:
             # You should always register all possible parameters, but the
             # optional ones can be None if you want.
@@ -39,68 +42,76 @@ class EnsembleLayer(nn.Module):
         if self.bias is not None:
             nn.init.uniform_(self.bias, -bound, bound)
 
-    def forward(self, input: torch.Tensor):
-        # input-size: batch-size * ensemble-size * input-dim
-        # weight: ensemble-size * input-dim * output-dim
-        out = torch.einsum("abc,bcd->abd", input, self.weight)
+    def forward(self, inputs: torch.Tensor):
+        # input-size: batch-size * ensemble-size * node-num * input-dim
+        # weight: ensemble-size * node-num * input-dim * output-dim
+        out = torch.einsum("abcd,bcde->abce", inputs, self.weight)
         if self.bias is not None:
             out += self.bias
 
         return out
 
     def __repr__(self):
-        return 'EnsembleLayer(ensemble_size={}, input_features={}, output_features={}, bias={})'.format(
-            self.ensemble_size, self.input_features, self.output_features,
-            self.bias is not None)
+        return 'EnsembleCausalLayer(ensemble_size={}, input_features={}, output_features={}, node_num={}, bias={})'.\
+            format(self.ensemble_size, self.input_features, self.output_features, self.node_num,
+                   self.bias is not None)
 
     def __str__(self):
-        return 'ensemble_size={}, input_features={}, output_features={}, bias={}'.format(
-            self.ensemble_size, self.input_features, self.output_features,
+        return 'ensemble_size={}, input_features={}, output_features={}, self.node_num, bias={}'.format(
+            self.ensemble_size, self.input_features, self.output_features, self.node_num,
             self.bias is not None)
 
 
-class EnsembleMLP(nn.Module, ABC):
+class EnsembleSparseMLP(nn.Module):
     def __init__(self,
                  input_dim,
                  output_dim,
+                 sparse_num,
                  hidden_dims,
                  activation='tanh',
                  last_activation='identity',
                  ensemble_size=7,
                  biases=None):
-        super(EnsembleMLP, self).__init__()
-        sizes_list = hidden_dims.copy()
+        super(EnsembleSparseMLP, self).__init__()
+        self.hidden_dims = [input_dim] + hidden_dims.copy() + [output_dim]
+
+        biases = [True] * (len(self.hidden_dims) - 1) if biases is None else biases.copy()
+        biases[0] = False
+
         self.activation = getattr(torch, activation)
         # setattr in __init__.py of models
         self.last_activation = getattr(torch, last_activation)
         self.ensemble_size = ensemble_size
-
-        sizes_list.insert(0, input_dim)
-        biases = [True] * len(sizes_list) if biases is None else biases.copy()
+        self.input_dim = input_dim
+        self.sparse_num = sparse_num
 
         layers = []
-        if 1 < len(sizes_list):
-            for i in range(len(sizes_list) - 1):
-                layers.append(EnsembleLayer(sizes_list[i], sizes_list[i + 1], ensemble_size, bias=biases[i]))
-        self.last_layer = EnsembleLayer(sizes_list[-1], output_dim, ensemble_size)
-        self.layers = nn.ModuleList(layers)
+        for l in range(len(self.hidden_dims) - 1):
+            layers.append(EnsembleSparseLayer(self.hidden_dims[l], self.hidden_dims[l + 1], node_num=sparse_num,
+                                              ensemble_size=ensemble_size, bias=biases[l]))
+        self.fc = nn.ModuleList(layers)
+
 
     def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
+        # x size: batch-size, ensemble-size, input-dim
+        x = torch.unsqueeze(x, 2)
+        x = x.repeat(1, 1, self.sparse_num, 1)
+        # x size: batch-size, ensemble-size, output-dim, input-dim
+        for fc in self.fc:
             x = self.activation(x)
-        x = self.last_layer(x)
+            x = fc(x)
+        # x size: batch-size, ensemble-size, node-num, 2
         x = self.last_activation(x)
         return x
 
-    def __str__(self):
-        string = ""
-        for layer in self.layers:
-            string += str(layer) + "\n"
-            string += str(self.activation) + "\n"
-        string += str(self.last_layer) + "\n"
-        string += str(self.last_activation)
-        return string
+    def compute_l1_loss(self):
+        """Take l1 norm of fc1 weight"""
+        # shape: ensemble-size, output-dim, input-dim, hidden-dim[0]
+        weight = self.fc[0].weight
+        weight = torch.abs(weight)
+        l1_obj, _ = torch.max(weight, dim=-1)
+        l1_obj = torch.sum(l1_obj)
+        return l1_obj
 
     def compute_l2_loss(self, l2_loss_coefs: Union[float, List[float]]):
         weight_norms = []
@@ -112,23 +123,25 @@ class EnsembleMLP(nn.Module, ABC):
         return weight_decay
 
 
-class EnsembleModel(EnsembleMLP):
+class EnsembleSparseModel(EnsembleSparseMLP):
     def __init__(self, state_dim: int, action_dim: int, reward_dim: int, hidden_dims: List[int],
                  output_state_dim=None, ensemble_size=7):
-        input_dim = state_dim + action_dim
+        self.input_dim = state_dim + action_dim
         output_state_dim = output_state_dim or state_dim
-        output_dim = (output_state_dim + reward_dim) * 2
+        self.output_dim = output_state_dim + reward_dim
 
         self.output_state_dim = output_state_dim
         self.ensemble_size = ensemble_size
-        super(EnsembleModel, self).__init__(input_dim, output_dim, hidden_dims, ensemble_size=ensemble_size)
+        super(EnsembleSparseModel, self).__init__(self.input_dim, 2, self.output_dim, hidden_dims,
+                                                  ensemble_size=ensemble_size)
 
     def forward(self, states, actions):
         # input size: batch-size * ensemble-size * dim
         inputs = torch.cat([states, actions], dim=-1)
         # print(inputs.shape)
-        outputs = super(EnsembleModel, self).forward(inputs)
-        # print(outputs.shape)
+        outputs = super(EnsembleSparseModel, self).forward(inputs)
+        batch_size = outputs.shape[0]
+        outputs = outputs.contiguous().view(batch_size, self.ensemble_size, 2 * self.output_dim)
         outputs = [{'diff_states': outputs[:, i, :self.output_state_dim * 2],
               'rewards': outputs[:, i, self.output_state_dim * 2:]}
              for i in range(self.ensemble_size)]
@@ -156,29 +169,32 @@ class EnsembleModel(EnsembleMLP):
         self.load_state_dict(new_state_dict)
 
 
-def test_ensemble_layer():
+def test_ensemble_sparse_layer():
     batch_size, ensemble_size, input_dim, output_dim = 128, 7, 32, 64
+    node_num = 10
 
-    inputs = torch.randn(batch_size, input_dim)
+    inputs = torch.randn(batch_size, node_num, input_dim)
     inputs = torch.unsqueeze(inputs, 1)
-    inputs = inputs.repeat([1, ensemble_size, 1])
-    weights = torch.rand(ensemble_size, input_dim, output_dim)
-    outputs = torch.einsum("abc,bcd->abd", inputs, weights)
+    inputs = inputs.repeat([1, ensemble_size, 1, 1])
+    weights = torch.rand(ensemble_size, node_num, input_dim, output_dim)
+    outputs = torch.einsum("abcd,bcde->abce", inputs, weights)
 
-    ensemble_layer = EnsembleLayer(input_dim, output_dim, ensemble_size=ensemble_size, bias=False)
+    ensemble_layer = EnsembleSparseLayer(input_dim, output_dim, ensemble_size=ensemble_size,
+                                         node_num=node_num, bias=False)
     ensemble_layer.weight.data[:] = weights
     output_torch = ensemble_layer(inputs)
 
     # compare
     print(torch.allclose(output_torch, outputs))
 
-def test_ensemble_mlp():
+
+def test_ensemble_sparse_mlp():
     batch_size = 256
     input_dim = 20
-    output_dim = 1
+    output_dim = 10
     hidden_dims = [100, 100]
-    ensemble_size = 5
-    model = EnsembleMLP(input_dim, output_dim, hidden_dims, ensemble_size=ensemble_size)
+    ensemble_size = 7
+    model = EnsembleSparseMLP(input_dim, 2, output_dim, hidden_dims, ensemble_size=ensemble_size)
 
     # print(model)
 
@@ -187,11 +203,15 @@ def test_ensemble_mlp():
     inputs = inputs.repeat([1, ensemble_size, 1])
 
     outputs = model(inputs)
+    l1_loss = model.compute_l1_loss()
+    l2_loss = model.compute_l2_loss([0.001, 0.001, 0.001])
 
     print(outputs.shape)
+    print(l1_loss)
+    print(l2_loss)
 
 
-def test_ensemble_model():
+def test_ensemble_sparse_model():
     import gym
     env = gym.make("CartPole-v1")
     state_dim = env.observation_space.shape[0]
@@ -200,17 +220,18 @@ def test_ensemble_model():
     hidden_dims = [200, 200]
     ensemble_size = 7
 
-    model = EnsembleModel(state_dim, action_dim, reward_dim, hidden_dims, ensemble_size)
+    model = EnsembleSparseModel(state_dim, action_dim, reward_dim, hidden_dims, ensemble_size)
 
-    state_dict = model.get_state_dict(2)
-    model.set_state_dict(2, state_dict)
+    batch_size = 256
+    sampled_states = torch.randn([batch_size, ensemble_size, state_dim])
+    sampled_actions = torch.randn([batch_size, ensemble_size, action_dim])
+
+    outputs = model(sampled_states, sampled_actions)
+    print(outputs[0]["diff_states"].shape)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     setattr(torch, 'identity', lambda x: x)
     setattr(torch, 'swish', lambda x: x * torch.sigmoid(x))
 
-    # test_ensemble_model()
-
-    test_ensemble_mlp()
+    test_ensemble_sparse_model()
